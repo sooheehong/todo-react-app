af57ace5d319018e0c8dc903cf2c87e987a1213e
sooheehong <sooheehong@kakao.com>
2021-10-28 21:32:22 +0900

    todo list 추가

--- src/App.js
+++ src/App.js
@@ -30,11 +30,22 @@ import Todo from './Todo';
 import './App.css'
 
 class App extends React.Component {
-    render(){
+	constructor(props){
+		super(props);
+		this.state = {
+			items: [
+				{ id: 0, title: 'Hello World 1', done: true},
+				{ id: 1, title: 'Hello World 2', done: false},
+			]
+		}
+	}
+    render() {
+		var todos = this.state.items.map((item, index) => (
+			<Todo item={item} key={item.id} />
+		));
         return (
             <div className="App">
-              <Todo />
-              <Todo />
+				{todos}
             </div>
         );
     }
