52c29438dfd63cd28e90f104408b3a08a7aafce7
sooheehong <sooheehong@kakao.com>
2021-11-06 21:45:58 +0900

    1) react-router-dom 추가 2) 로그인 페이지 구현 3) 미인증 시 로그인페이지로 리다이렉트 추가

---
+++ src/AppRouter.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "./index.css"
+import App from "./App";
+import Login from "./Login";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Typography } from "@material-ui/core";
+import { Box } from "@material-ui/core";
+
+function Copyright() {
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {"Copyright ⓒ"}
+            fsoftwareengineer, {new Date().getFullYear()}
+            {"."}
+        </Typography>
+    );
+}
+
+class AppRouter extends React.Component {
+    render() {
+        return (
+            <div>
+                <Router>
+                    <div>
+                        <Switch>
+                            <Route path="/login">
+                                <Login />
+                            </Route>
+                            <Route path="/">
+                                <App />
+                            </Route>
+                        </Switch>
+                    </div>
+                    <Box mt={5}>
+                        <Copyright />
+                    </Box>
+                </Router>
+            </div>
+        )
+    }
+}
+export default AppRouter;
--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -3,8 +3,8 @@ import { API_BASE_URL  } from "../api-config";
 export function call(api, method, request) {
     let options = {
         headers: new Headers({
-            "Content-Type": "application/json",
-            'Accept': 'application/json'
+            // "Content-Type": "application/json",
+            // 'Accept': 'application/json'
         }),
         url: API_BASE_URL + api,
         method: method
@@ -22,4 +22,11 @@ export function call(api, method, request) {
             return json;
         })    
     )
+    .catch((error) => {
+        console.log(error.status);
+        if(error.status == 403) {
+            window.location.href = "/login";
+        }
+        return Promise.reject(error);
+    });
 }
